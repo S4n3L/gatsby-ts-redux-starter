2f5aac4e3340745929b19d1efce7b88e17c45b8b
S4n3L <mr.szabolcs.farkas@gmail.com>
2020-02-19 14:17:27 +0100

    Typescript + css modules + eslint is doen

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -12,7 +12,8 @@ import { useStaticQuery, graphql } from "gatsby"
 import Header from "./header"
 import "./layout.css"
 
-const Layout = ({ children }) => {
+const Layout: React.FunctionComponent = (props) => {
+  const { children } = props
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
@@ -28,15 +29,15 @@ const Layout = ({ children }) => {
       <Header siteTitle={data.site.siteMetadata.title} />
       <div
         style={{
-          margin: `0 auto`,
+          margin: "0 auto",
           maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
+          padding: "0 1.0875rem 1.45rem",
         }}
       >
         <main>{children}</main>
         <footer>
           © {new Date().getFullYear()}, Built with
-          {` `}
+          {" "}
           <a href="https://www.gatsbyjs.org">Gatsby</a>
         </footer>
       </div>
